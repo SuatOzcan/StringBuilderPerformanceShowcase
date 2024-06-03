9e4785c9878fc7619157235b86bd41f3e0eca106
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-06-03 14:38:55 +0300

    Add project files.

---
+++ StringBuilderPerformanceShowcase/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Text;
+
+const int Count = 100_000;
+
+string result = string.Empty;
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+for (int i = 0; i < Count; i++)
+{
+    result += 'a';
+}
+
+stopwatch.Stop();
+Console.WriteLine($"The elapsed time with string appending is {stopwatch.Elapsed}");
+
+StringBuilder sb = new StringBuilder(Count);
+
+stopwatch.Restart();
+for (int i = 0; i < Count; i++)
+{
+    sb.Append('a');
+}
+stopwatch.Stop();
+
+Console.WriteLine($"The elapsed time with string builder is {stopwatch.Elapsed}");
+Console.WriteLine("Wow! This is fast!");
